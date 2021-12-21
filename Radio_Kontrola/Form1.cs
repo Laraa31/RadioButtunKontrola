29b8c2abc69bb3f9b41e1bf1ec1170e3556ed168
Laraa31 <88662021+Laraa31@users.noreply.github.com>
2021-12-21 12:37:17 +0100

    Add files via upload

---
+++ Radio_Kontrola/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Radio_Kontrola
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void radioButton_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonRezerviraj_Click(object sender, EventArgs e)
+        {
+            if (radioButtonAutobus.Checked)
+            {
+                textBox1.Text = "Odabrali ste prijevoz autobusom.";
+
+            }
+
+            if (radioButtonAvion.Checked)
+            {
+                textBox1.Text = "Odabrali ste prijevoz avionom.";
+            }
+
+            if (radioButtonVlastiti.Checked){
+                textBox1.Text = "Odabrali ste vlastiti prijevoz.";
+            }
+        }
+    }
+}
